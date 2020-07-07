cc4635ad730d8145eff7d8c90e63fc4155277b56
joycenerd <joycenerd@csie.io>
2020-07-07 20:42:30 +0800

    try rmsprop

--- options.py
+++ options.py
@@ -9,7 +9,7 @@ parser.add_argument('--data_root',type=str,default=Path(ROOTPATH).joinpath('C1-P
 parser.add_argument('--model',type=str,default="efficientnet-b5",help="which model: [resenest50, resnest101, resnest200, resnest269, efficientnet-b4]")
 parser.add_argument('--lr',type=float,default=0.005,help="learning rate")
 parser.add_argument('--cuda_devices',type=int,default=0,help='gpu device')
-parser.add_argument('--epochs',type=int,default=100,help='num of epoch')
+parser.add_argument('--epochs',type=int,default=200,help='num of epoch')
 parser.add_argument('--num_classes',type=int,default=3,help='The number of classes for your classification problem')
 parser.add_argument('--train_batch_size',type=int,default=4,help='The batch size for training data')
 parser.add_argument('--num_workers',type=int,default=4,help='The number of worker while training')
