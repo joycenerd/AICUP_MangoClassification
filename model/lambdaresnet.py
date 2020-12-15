37666c6d98392118619ab500bdb54ebce9951f6b
joycenerd <joycenerd@csie.io>
2020-12-15 11:42:34 +0800

    finish LambdaResnet

--- model/lambdaresnet.py
+++ model/lambdaresnet.py
@@ -4,6 +4,14 @@ import torch.nn.functional as F
 
 
 class LambdaConv(nn.Module):
+    ''' Lambda convolution layer
+    :param in_channels: input channel size
+    :param out_channels: output channel size
+    :param heads: multi-head query
+    :param k: key depth
+    :param u: intra-depth
+    :param m: number of context position 
+    '''
     def __init__(self, in_channels, out_channels, heads=4, k=16, u=1, m=23):
         super(LambdaConv, self).__init__()
         self.kk, self.uu, self.vv, self.mm, self.heads = k, u, out_channels // heads, m, heads
@@ -27,9 +35,8 @@ class LambdaConv(nn.Module):
         if self.local_context:
             self.embedding = nn.init.kaiming.normal_(torch.empty(self.kk, self.uu, 1, m, m), model='fan_out', nonlinearity='relu')
         else:
-            self.embedding = nn.init.kaiming.normal_(torch.empty(self.kk, self.uu), mode='fan_out', nonlinearity='rele')
+            self.embedding = nn.init.kaiming.normal_(torch.empty(self.kk, self.uu), mode='fan_out', nonlinearity='relu')
 
-    
     def forward(self,x):
         n_batch, C, w, h = x.size()
         queries = self.queries(x).view(n_batch, self.heads, self.kk, w * h)
@@ -48,3 +55,118 @@ class LambdaConv(nn.Module):
         out = y_c + y_p
         out = out.contiguous().view(n_batch, -1, w, h)
         return out
+
+
+class LambdaBottleneck(nn.Module):
+    '''Lambda bottleneck layer
+    :param in_planes: input channel size
+    :param planes: output channel size
+    :param stride: stride size
+    '''
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(LambdaBottleneck,self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.ModuleList([LambdaConv(planes, planes)])
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.conv2.append(nn.AvgPool2d(kernel_size=(3,3), stride=stride, padding=(1,1)))
+        self.conv2.append(nn.BatchNorm2d(planes))
+        self.conv2.append(nn.ReLU())
+        self.conv2 = nn.Sequential(*self.conv2)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+    
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.conv2(out)
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    '''ResNet
+    :param block: Bottleneck layer
+    :param num_blocks: number repetitive bottleneck layer
+    :param num_classes: output class size
+    '''
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet,self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0])
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.3),
+            nn.Linear(512*block.expansion, num_classes)
+        )
+    
+    def _make_layer(self, block, planes, num_blocks, stride=1):
+        ''' make convolution block
+        :param block: bottleneck layer
+        :param planes: output channel size
+        :param stride: stride size
+        :return covolution block
+        '''
+        strides = [stride] + [1] * (num_blocks-1)
+        layers = []
+        for idx, stride in enumerate(strides):
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.maxpool(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
+
+def LambdaResNet50(num_classes):
+    return ResNet(LambdaBottleneck, [3, 4, 6, 3], num_classes)
+
+
+def LambdaResNet101(num_classes):
+    return ResNet(LambdaBottleneck,[3, 4, 23, 3], num_classes)
+
+
+def LambdaResNet152(num_classes):
+    return ResNet(LambdaBottleneck, [3, 8, 36, 3], num_classes)
+
+
+def LambdaResNet200(num_classes):
+    return ResNet(LambdaBottleneck, [3, 24, 36, 3], num_classes)
+
+
+def LambdaResNet270(num_classes):
+    return ResNet(LambdaBottleneck, [4, 29, 53, 4], num_classes)
+
+
+def LambdaResNet350(num_classes):
+    return ResNet(LambdaBottleneck, [4, 36, 72, 4], num_classes)
+
+
+def LambdaResNet420(num_classes):
+    return ResNet(LambdaBottleneck, [4, 44, 87, 4], num_classes)
--- model/model_utils.py
+++ model/model_utils.py
@@ -1,6 +1,7 @@
 from .resnest.restnest import get_model
 from efficientnet_pytorch import EfficientNet
 from options import opt
+from .lambdaresnet import LambdaResNet50, LambdaResNet101, LambdaResNet152, LambdaResNet200, LambdaResNet270, LambdaResNet350, LambdaResNet420
 
 def get_net(model):
     if model[0:8]=='resnest':
@@ -17,4 +18,17 @@ def get_net(model):
     elif model == 'efficientnet-b3':
         model = EfficientNet.from_pretrained('efficientnet-b3', num_classes = opt.num_classes)
         return model
-    
+    elif model == 'lambdaresnet50':
+        model = LambdaResNet50(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet101':
+        model = LambdaResNet101(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet152':
+        model = LambdaResNet152(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet200':
+        model = LambdaResNet200(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet270':
+        model = LambdaResNet270(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet350':
+        model = LambdaResNet350(num_classes=opt.num_classes)
+    elif model == 'lambdaresnet420':
+        model = LambdaResNet420(num_classes=opt.num_classes)
